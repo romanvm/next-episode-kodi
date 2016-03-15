563377ca5345d840431f99ce5090d8f461bf1319
Roman Miroshnychenko <romanvm@yandex.ru>
2016-03-15 22:44:08 +0200

    Adds correct finctions for next-episode API

--- script.service.next-episode/libs/nextepisode.py
+++ script.service.next-episode/libs/nextepisode.py
@@ -3,20 +3,52 @@
 # Author: Roman Miroshnychenko aka Roman V.M. (romanvm@yandex.ua)
 
 import json
-import requests
+import urllib2
+from contextlib import closing
 
-API = 'https://next-episode.net/api/kodi/v1/update_data'
+UPDATE_DATA = 'https://next-episode.net/api/kodi/v1/update_data'
+LOGIN = 'https://next-episode.net/api/kodi/v1/login'
 
 
-def send_data(data):
+def web_client(url, data=None):
     """
-    Send data to next-episode.net
+    Send/receive data to/from next-episode.net
 
-    :param data: data to be sent
+    :param url: url to open
+    :type url: str
+    :param data: data to be sent in a POST request
     :type data: dict
     """
-    resp = requests.get(API, params={'data': json.dumps(data)}, headers={'Content-Type': 'application/json'})
-    return resp.text
+    if data is not None:
+        data = json.dumps(data)
+    request = urllib2.Request(url, data, headers={'Content-Type': 'application/json'})
+    with closing(urllib2.urlopen(request)) as response:
+        return response.read()
+
+
+def update_data(data):
+    """
+    Update movies/tvshows data
+
+    :param data: data to be send to next-episode.net
+    :type data: dict
+    :return: next-episode.net response
+    """
+    return web_client(UPDATE_DATA, data)
+
+
+def get_password_hash(username, password):
+    """
+    Get password hash from next-episode.net
+
+    :param username: next-episode username
+    :type username: str
+    :param password: next-episode password
+    :type password: str
+    :return: password hash
+    :rtype: str
+    """
+    return json.loads(web_client(LOGIN, {'username': username, 'password': password}))['hash']
 
 
 if __name__ == '__main__':
@@ -30,4 +62,7 @@ if __name__ == '__main__':
         'movies': [
             {'imdb_id': 'tt1431045', 'watched': '1'},
         ]}
-    print send_data(data)
+
+    login = {'username': 'test','password': 'test123'}
+    print update_data(data)
+    print get_password_hash(login['username'], login['password'])
