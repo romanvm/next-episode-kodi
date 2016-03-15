d46ac5e5da72be65718aefe4ede6a8f06d908bc4
Roman Miroshnychenko <romanvm@yandex.ru>
2016-03-15 22:44:42 +0200

    Adds json_request function for communicating with Kodi

--- script.service.next-episode/libs/medialibrary.py
+++ script.service.next-episode/libs/medialibrary.py
@@ -2,4 +2,25 @@
 # Created on: 15.03.2016
 # Author: Roman Miroshnychenko aka Roman V.M. (romanvm@yandex.ua)
 
+import sys
+if sys.version_info[1] >= 7:
+    import json
+else:
+    import simplejson as json
+import xbmc
 
+
+def json_request(method, params):
+    """
+    Send JSON-RPC to Kodi
+
+    :param method: Kodi JSON-RPC method
+    :type method: str
+    :param params: method parameters
+    :type params: dict
+    :return: JSON-RPC response
+    :rtype: dict
+    """
+    return json.loads(xbmc.executeJSONRPC(json.dumps({
+        'jsonrpc': '2.0', 'method': method, 'params': params, 'id': '1'
+    })))
