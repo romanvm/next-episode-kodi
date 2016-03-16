f22b573e5ba9011ad8392e51ac52ba1bb02fb059
Roman Miroshnychenko <romanvm@yandex.ua>
2016-03-16 13:47:31 +0200

    Adds get_movies, get_tvshows and get_episodes functions to medialibrary module

--- script.service.next-episode/libs/medialibrary.py
+++ script.service.next-episode/libs/medialibrary.py
@@ -20,3 +20,41 @@ def json_request(method, params):
     return json.loads(xbmc.executeJSONRPC(json.dumps({
         'jsonrpc': '2.0', 'method': method, 'params': params, 'id': '1'
     })))
+
+
+def get_movies():
+    """
+    Get the list of movies from the Kodi database
+
+    :return: the list of movie data as Python dicts like this:
+        ``{u'imdbnumber': u'tt1267297', u'playcount': 0, u'movieid': 2, u'label': u'Hercules'}``
+    :rtype: list
+    """
+    params = {'properties': ['imdbnumber', 'playcount'], 'sort': {'order': 'ascending', 'method': 'label'}}
+    return json_request('VideoLibrary.GetMovies', params)['movies']
+
+
+def get_tvshows():
+    """
+    Get te list of TV shows from the Kodi database
+
+    :return: the list of TV show data as Python dicts like this:
+        {u'imdbnumber': u'247897', u'tvshowid': 3, u'label': u'Homeland'}
+    :rtype: list
+    """
+    params = {'properties': ['imdbnumber'], 'sort': {'order': 'ascending', 'method': 'label'}}
+    return json_request('VideoLibrary.GetTVShows', params)['tvshows']
+
+
+def get_episodes(tvshowid):
+    """
+    Get the lis of episodes from a specific TV show
+
+    :param tvshowid: internal Kodi database ID for a TV show
+    :type tvshowid: str
+    :return: the liso of episode data as Python dicts like this:
+        ``{u'season': 4, u'playcount': 0, u'episode': 1, u'episodeid': 5, u'label': u'4x01. The Drone Queen'}``
+    :rtype: list
+    """
+    params = {'tvshowid': tvshowid, 'properties': ['season', 'episode', 'playcount']}
+    return json_request('VideoLibrary.GetEpisodes', params)['episodes']
