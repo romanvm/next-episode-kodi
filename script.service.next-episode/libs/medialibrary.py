c2f799bc04b120ffaf5347576a6a1ad081db3b64
Roman Miroshnychenko <romanvm@yandex.ua>
2016-03-21 14:21:43 +0200

    Adds "next-episode-net:" to log messages for JSON-RPC

--- script.service.next-episode/libs/medialibrary.py
+++ script.service.next-episode/libs/medialibrary.py
@@ -25,9 +25,9 @@ def send_json_rpc(method, params=None):
     if params is not None:
         request['params'] = params
     json_request = json.dumps(request)
-    xbmc.log('JSON-RPC request: {0}'.format(json_request), xbmc.LOGNOTICE)
+    xbmc.log('next-episode-net: JSON-RPC request: {0}'.format(json_request), xbmc.LOGNOTICE)
     json_reply = xbmc.executeJSONRPC(json_request)
-    xbmc.log('JSON-RPC reply: {0}'.format(json_reply), xbmc.LOGNOTICE)
+    xbmc.log('next-episode-net: JSON-RPC reply: {0}'.format(json_reply), xbmc.LOGNOTICE)
     return json.loads(json_reply)['result']
 
 
